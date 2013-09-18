d4c71daec17ccc81c19ac3367a0562cc97442b5e
acm46 <anirudh.c.mohan@gmail.com>
2013-09-18 03:25:08 -0400

    fixed alignment issues

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
 	$('a').smoothScroll();
+
+	function abso() {
+
+        $('.newsect').css({
+            width: $(window).width(),
+            height: $(window).height()-70
+        });
+
+    }
+
+    $(window).resize(function() {
+        abso();         
+    });
+
+    abso();
+
 });
